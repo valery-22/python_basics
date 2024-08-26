d4cd134f203e7a0c7dcc59b2b20410aa23f92cfc
Valery Hoyos <73263344+valery-22@users.noreply.github.com>
2024-08-25 22:31:05 -0500

    first commit

---
+++ area_of_circle.py
@@ -0,0 +1,5 @@
+# Update your code below this line
+pi = 3.14
+radius = 8.9
+area_of_circle = (pi * radius * radius)
+print(area_of_circle)
---
+++ area_of_rectangle.py
@@ -0,0 +1,4 @@
+length = 45
+width = 76
+area_of_rectangle = (length * width)
+print(area_of_rectangle)
